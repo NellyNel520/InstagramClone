e9ac4d0f26a125c2ca82f800c76f920c089c60dc
NellyNel520 <channelharris520@gmail.com>
2023-07-13 16:47:20 -0400

    completed post router

--- api/routes/AppRouter.js
+++ api/routes/AppRouter.js
@@ -2,13 +2,15 @@ const Router = require("express").Router();
 const UserRouter = require("./UserRouter");
 const AuthRouter = require("./AuthRouter");
 const CommentRouter = require("./CommentRouter");
-// const middleware = require("../middleware");
+const PostRouter = require("./PostRouter");
+const middleware = require("../middleware");
 
 
 
 
 
 Router.use("/users", UserRouter);
+Router.use("/posts", PostRouter);
 Router.use("/comments", CommentRouter);
 Router.use("/auth", AuthRouter);
 
---
+++ api/routes/PostRouter.js
@@ -0,0 +1,13 @@
+const Router = require("express").Router();
+
+const controller = require("../controllers/PostController");
+
+
+Router.get("/all", controller.GetAllPost);
+Router.post("/new", controller.CreatePost);
+Router.get("/byUserId/:user_id", controller.GetPostByUserId);
+Router.get("/byId/:id", controller.GetPostById);
+Router.put("/update/:id", controller.UpdatePost);
+Router.delete("/:postId", controller.DeletePost);
+
+module.exports = Router;
