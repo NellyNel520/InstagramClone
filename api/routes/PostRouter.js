bd9fa2a9d3ccbd40949f818ebe46824a64dd8a0b
NellyNel520 <channelharris520@gmail.com>
2023-07-13 17:55:32 -0400

    corrected errors to get server running

--- api/routes/PostRouter.js
+++ api/routes/PostRouter.js
@@ -7,7 +7,7 @@ Router.get("/all", controller.GetAllPost);
 Router.post("/new", controller.CreatePost);
 Router.get("/byUserId/:user_id", controller.GetPostByUserId);
 Router.get("/byId/:id", controller.GetPostById);
-Router.put("/update/:id", controller.Up);
+Router.put("/update/:id", controller.UpdatePost);
 Router.delete("/:postId", controller.DeletePost);
 
 module.exports = Router;
