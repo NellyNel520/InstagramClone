8c1c72bd0302047263ae8d00581f37a50d62ac1a
NellyNel520 <channelharris520@gmail.com>
2023-07-14 16:13:35 -0400

    added icons png files to assets folder

--- instaClone/components/home/Header.js
+++ instaClone/components/home/Header.js
@@ -1,14 +1,40 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const Header = () => {
-  return (
-    <View>
-      <Image style={styles.logo} source={require('../../assets/ig-logo.png')}/>
-    </View>
-  )
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity>
+				<Image
+					style={styles.logo}
+					source={require('../../assets/ig-logo.png')}
+				/>
+			</TouchableOpacity>
+
+      <View style={styles.iconsContainer}>
+        <Text style={{ color: 'white' }}>Dummy </Text>
+        <Text style={{ color: 'white' }}>Dummy </Text>
+        <Text style={{ color: 'white' }}>Dummy </Text>
+      </View>
+		</View>
+	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginHorizontal: 20,
+  },
+  iconsContainer: {
+    flexDirection: 'row',
+  },
+	logo: {
+		width: 100,
+		height: 50,
+		resizeMode: 'contain',
+	},
+})
 
 export default Header
