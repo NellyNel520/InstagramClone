d7c49e90d389cff798c49f1d94c89a15a72a6fd5
NellyNel520 <channelharris520@gmail.com>
2023-07-14 16:32:08 -0400

    added add, like, and messenger icons to header for home page / feed

--- instaClone/components/home/Header.js
+++ instaClone/components/home/Header.js
@@ -11,25 +11,47 @@ const Header = () => {
 				/>
 			</TouchableOpacity>
 
-      <View style={styles.iconsContainer}>
-        <Text style={{ color: 'white' }}>Dummy </Text>
-        <Text style={{ color: 'white' }}>Dummy </Text>
-        <Text style={{ color: 'white' }}>Dummy </Text>
-      </View>
+			<View style={styles.iconsContainer}>
+				<TouchableOpacity>
+					<Image
+						style={styles.icon}
+						source={require('../../assets/icons/add-icon.png')}
+					/>
+				</TouchableOpacity>
+        <TouchableOpacity>
+					<Image
+						style={styles.icon}
+						source={require('../../assets/icons/heart-icon.png')}
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity>
+					<Image
+						style={styles.icon}
+						source={require('../../assets/icons/messenger-icon.png')}
+					/>
+				</TouchableOpacity>
+				
+			</View>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-    marginHorizontal: 20,
-  },
-  iconsContainer: {
-    flexDirection: 'row',
-  },
+	container: {
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		flexDirection: 'row',
+		marginHorizontal: 20,
+	},
+	iconsContainer: {
+		flexDirection: 'row',
+	},
+	icon: {
+		width: 30,
+		height: 30,
+		marginLeft: 10,
+		resizeMode: 'contain',
+	},
 	logo: {
 		width: 100,
 		height: 50,
