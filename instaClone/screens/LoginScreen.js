3821dae105bb5f5f67f5fd5b78f1f640d5d48487
NellyNel520 <channelharris520@gmail.com>
2023-07-17 21:56:03 -0400

    created login screen

--- instaClone/screens/LoginScreen.js
+++ instaClone/screens/LoginScreen.js
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 
-const LoginScreen = () => {
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+const INSTAGRAM_LOGO = 'https://img.icons8.com/fluency/48/instagram-new.png'
 
+const LoginScreen = () => (
+  <View style={styles.container}>
+    <View style={styles.logoContainer}>
+      <Image source={{uri: INSTAGRAM_LOGO, height: 100, width: 100}}/>
+    </View>
+    {/* Login Form section */}
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 50,
+    paddingHorizontal: 12,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginTop: 60,
+  }
+})
 export default LoginScreen
